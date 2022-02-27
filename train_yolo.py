c675772032188b4600b2147519b6523e99b53762
Ravi Dudhagra <rdudhagra@gmail.com>
2022-02-27 00:52:19 -0500

    Modified yolo params to run on NVIDIA RTX2080B - Larger batch size apparently performs better (https://github.com/ultralytics/yolov5/issues/2377)

--- train_yolo.py
+++ train_yolo.py
@@ -5,9 +5,9 @@ DATASET_NAME = "breaker"
 
 if __name__ == "__main__":
     train.run(
-        imgsz=1280,
+        imgsz=640,
         data=os.path.join("data", DATASET_NAME, f"{DATASET_NAME}.yaml"),
         epochs=30,
-        batch=16,
+        batch=128,
         weights="yolov5m.pt",
     )
