2e1df19abb9664418353e9d6023198699755628b
Ravi Dudhagra <rdudhagr@ghc80.ghc.andrew.cmu.edu>
2022-02-27 17:48:45 -0500

    Results from overnight train

--- train_yolo.py
+++ train_yolo.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     train.run(
         imgsz=640,
         data=os.path.join("data", DATASET_NAME, f"{DATASET_NAME}.yaml"),
-        epochs=30,
+        epochs=500,
         batch=128,
         weights="yolov5m.pt",
     )
